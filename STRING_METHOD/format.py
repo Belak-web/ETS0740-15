88cdc640e839f92081688e9a7b5693105903bc0c
Belak-web <143265275+Belak-web@users.noreply.github.com>
2025-03-25 18:52:50 +0300

    Create format.py

---
+++ STRING_METHOD/format.py
@@ -0,0 +1,8 @@
+name = "Kaleb"
+age = 21
+message = "My name is {0} and I am {1} years old. {1} is my favorite number.".format(
+    name, age)
+print(message)
+# output: My name is kaleb and I am 21 years old. 22 is my favorite number.
+
+
