90f49caf30f4305231e65c16464af756267b5200
Belak-web <143265275+Belak-web@users.noreply.github.com>
2025-04-20 16:51:18 +0300

    Create update().py

---
+++ Dictionary methods/update().py
@@ -0,0 +1,8 @@
+car = {
+  "brand": "Bmw",
+  "model": "Competition",
+  "year": 1999
+}
+car.update({"color": "red"})
+print(car)
+#output {'brand': 'Bmw', 'model': 'Competition', 'year': 1999, 'color': 'red'}
